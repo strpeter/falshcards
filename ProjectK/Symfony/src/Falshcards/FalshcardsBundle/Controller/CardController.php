ceb6ce9c00004ad9df12ca19d1c5b06c6587a1e8
strpeter <bond-ps@hotmail.com>
2012-12-20 21:38:20 +0100

    Extended controller's features
    I added some features to stay most of the time at a place where one can
    use the menu.

--- ProjectK/Symfony/src/Falshcards/FalshcardsBundle/Controller/CardController.php
+++ ProjectK/Symfony/src/Falshcards/FalshcardsBundle/Controller/CardController.php
@@ -17,21 +17,23 @@ class CardController extends Controller
 	}
 	
 	// gibt die Form aus, um Karten hinzuzufuegen
-    public function showAddCardAction() {
+	public function showAddCardAction() {
 		// alle Ordner aus der Datenbank holen
 		$folders = $this->getDoctrine()->getRepository('FalshcardsBundle:Folder')->findAll();
         return $this->render('FalshcardsBundle:Card:addcardform.html.twig', array('folders' => $folders));
-    }
-    
-    // speichert die hinzugefuegte Karte in der DB
-    public function saveAddCardAction() {
+	}
+	
+	// speichert die hinzugefuegte Karte in der DB
+	public function saveAddCardAction() {
 		// den HTTP Request in eine Variable holen
 		$request = $this->getRequest();
+		
 		// wurde ich mit POST aufgerufen?
 		if ($request->getMethod() == 'POST') {
 			// wenn ja
 			// leere neue Karte erstellen
 			$card = new Card();
+			
 			// Frage und Antwort und Ordner aus dem Formular in das Objekt speichern
 			$card->setQuestion($request->request->get('question'));
 			$card->setAnswer($request->request->get('answer'));
@@ -39,12 +41,14 @@ class CardController extends Controller
 			if (!$folder)
 				trigger_error('Der ausgewaehlte Ordner konnte nicht gefunden werden!');
 			$card->setffolder($folder);
+			
 			// das Objekt $card in der Datenbank speichern
 			$em = $this->getDoctrine()->getEntityManager();
 			$em->persist($card);
 			$em->flush();
+			
 			// vielen dank anzeigen
-			return new Response('Karte hinzugefuegt, danke!');
+			return $this->render('FalshcardsBundle:Card:addcardform-dank.html.twig');
 		} else {
 			// wenn nein -> fehler!
 			trigger_error('Das muss mit einem HTTP POST aufgerufen werden!');
@@ -52,20 +56,22 @@ class CardController extends Controller
 	}
 	
 	// gibt die Form aus, um Karten zu aendern
-    public function showChangeCardAction($id_card) {
+	public function showChangeCardAction($id_card) {
 		// alle Ordner aus der Datenbank holen
 		$folders = $this->getDoctrine()->getRepository('FalshcardsBundle:Folder')->findAll();
+		
 		// karte aus der DB holen
 		$card = $this->getDoctrine()->getRepository('FalshcardsBundle:Card')->find($id_card);
 		if (!$card)
 			trigger_error('Karte nicht gefunden!');
-        return $this->render('FalshcardsBundle:Card:changecardform.html.twig', array('card' => $card, 'folders' => $folders));
-    }
+		return $this->render('FalshcardsBundle:Card:changecardform.html.twig', array('card' => $card, 'folders' => $folders));
+	}
     
-    // speichert die geaenderte Karte in der DB
-    public function saveChangeCardAction($id_card) {
+	// speichert die geaenderte Karte in der DB
+	public function saveChangeCardAction($id_card) {
 		// den HTTP Request in eine Variable holen
 		$request = $this->getRequest();
+		
 		// wurde ich mit POST aufgerufen?
 		if ($request->getMethod() == 'POST') {
 			// wenn ja
@@ -73,6 +79,7 @@ class CardController extends Controller
 			$card = $this->getDoctrine()->getRepository('FalshcardsBundle:Card')->find($id_card);
 			if (!$card)
 				trigger_error('Karte nicht gefunden!');
+			
 			// Frage und Antwort und Ordner aus dem Formular in das Objekt speichern
 			$card->setQuestion($request->request->get('question'));
 			$card->setAnswer($request->request->get('answer'));
@@ -80,12 +87,14 @@ class CardController extends Controller
 			if (!$folder)
 				trigger_error('Der ausgewaehlte Ordner konnte nicht gefunden werden!');
 			$card->setffolder($folder);
+			
 			// das Objekt $card in der Datenbank speichern
 			$em = $this->getDoctrine()->getEntityManager();
 			$em->persist($card);
 			$em->flush();
+			
 			// vielen dank anzeigen
-			return new Response('Karte geaendert, danke!');
+			return $this->render('FalshcardsBundle:Card:changecardform-dank.html.twig');
 		} else {
 			// wenn nein -> fehler!
 			trigger_error('Das muss mit einem HTTP POST aufgerufen werden!');
@@ -93,27 +102,31 @@ class CardController extends Controller
 	}
 	
 	// gibt die Form aus, um Ordner hinzuzufuegen
-    public function showAddFolderAction() {
-        return $this->render('FalshcardsBundle:Card:addfolderform.html.twig');
-    }
-    
-    // speichert den neuen Ordner
-    public function saveAddFolderAction() {
+	public function showAddFolderAction() {
+		return $this->render('FalshcardsBundle:Card:addfolderform.html.twig');
+	}
+	
+	// speichert den neuen Ordner
+	public function saveAddFolderAction() {
 		// den HTTP Request in eine Variable holen
 		$request = $this->getRequest();
+		
 		// wurde ich mit POST aufgerufen?
 		if ($request->getMethod() == 'POST') {
 			// wenn ja
 			// leeren neuen Ordner erstellen
 			$folder = new Folder();
+			
 			// Frage und Antwort aus dem Formular in das Objekt speichern
 			$folder->setName($request->request->get('name'));
+			
 			// das Objekt $folder in der Datenbank speichern
 			$em = $this->getDoctrine()->getEntityManager();
 			$em->persist($folder);
 			$em->flush();
+			
 			// vielen dank anzeigen
-			return new Response('Ordner hinzugefuegt, danke!');
+			return $this->render('FalshcardsBundle:Card:addfolderform-dank.html.twig');
 		} else {
 			// wenn nein -> fehler!
 			trigger_error('Das muss mit einem HTTP POST aufgerufen werden!');
@@ -133,6 +146,7 @@ class CardController extends Controller
 		$folder = $this->getDoctrine()->getRepository('FalshcardsBundle:Folder')->find($id_folder);
 		if (!$folder)
 			trigger_error('Ordner nicht gefunden!');
+
 		// karten des ordners aus der DB holen
 		$cards = $this->getDoctrine()->getRepository('FalshcardsBundle:Card')->findBy(array('ffolder' => $folder));
 		return $this->render('FalshcardsBundle:Card:showcards.html.twig', array('folder' => $folder, 'cards' => $cards));
@@ -143,15 +157,16 @@ class CardController extends Controller
 		// $base_path zeigt auf Symfony/web
 		$base_path = $this->get('kernel')->getRootDir();;
 		$tex_path = $base_path.'/../web/tex/';
-		$tex_file = 'card'; //remember you add '.tex' below!!!
-		$pdf_file = 'card.pdf';
-		$log_file = 'card.log';
+		$tex_file = $_SERVER["REMOTE_ADDR"];
+		$pdf_file = $tex_file.'.pdf';
+		$log_file = $tex_file.'.log';
+		
 		
 		// tex, pdf und log dateien loeschen
 		if (file_exists($tex_path.$tex_file.'.tex')) unlink($tex_path.$tex_file.'.tex');
 		if (file_exists($tex_path.$pdf_file)) unlink($tex_path.$pdf_file);
-		if (file_exists($tex_path.$log_file)) unlink($tex_path.$log_file);
-		
+		if (file_exists($tex_path.$log_file)) unlink($tex_path.$log_file);		
+
 		// tex-datei schreiben
 		$fh = fopen($tex_path.$tex_file.'.tex', 'w') or die("can't open file");
 		$tex = $this->renderView('FalshcardsBundle:Card:export.tex.twig');
@@ -159,7 +174,7 @@ class CardController extends Controller
 		fclose($fh);
 		
 		// tex datei kompilieren
-		$response = exec('cd '.$tex_path.'; pdflatex '.$tex_file.'.tex');
+		$response = exec('cd '.$tex_path.'; pdflatex '.$tex_file.'.tex > test.txt');
 		
 		// existiert das PDF-file?
 		if (file_exists($tex_path.$pdf_file)) {
